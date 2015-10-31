8c2cce437d59dec30fe37aa733a96cb88ce4b072
BenGoodwin25 <goodb25@gmail.com>
2015-10-31 10:11:24 +0100

    added subst_decrypt and finished subst_encrypt, also adding error check for the other *_encrypt & *_decrypt

---
+++ secu/subst/subst_decrypt.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void deleteDoubloon(char *self){
+	size_t size=strlen(self);
+	int i,j,k=0;
+	for(i=0;i<size-1;i++){
+		for(j=i+1;j<size-1;j++){
+			if(self[i]==self[j] && j<size-1){
+				self[k]==self[i];
+				memmove( self+j,self+j+1, (size-j* sizeof *self));
+				j+=1;
+			}
+		}
+	}
+}
+
+void createSubstTab(char *tabCipher,char *tabinit,char *key, size_t size){
+	int i;
+	for(i=0;i<size;i++){
+		tabCipher[i]=key[i];
+	}
+	for(i=size;i<26+size;i++){
+		tabCipher[i]=tabinit[i-size];
+	}
+	deleteDoubloon(tabCipher);
+}
+
+int isintab(char c, char *chars, int nb){
+	int i;
+	for(i=0;i<nb;i++){
+		if(chars[i] == c){
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+int main(int argc, char *argv[]) {
+	if(argc != 2){
+		fprintf(stderr,"Need one Parameter\n");
+		return 1;
+	}
+	int i,j;
+	char tabinit[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	char *tabCipher=malloc(sizeof(int)*26);
+	size_t size;
+	char c;
+	char *key=argv[1];
+	size=strlen(key);
+	createSubstTab(tabCipher, tabinit, key, size);
+	//DEBUG//
+	/*for(j=0;j<size+26;j++){
+		printf("%c :",tabinit[j]);
+		printf(" %c\n",tabCipher[j]);
+	}
+	printf("\n");*/
+	//ENCRYPT//
+	while ((i = fgetc(stdin)) != EOF){
+		if((unsigned char) i < 'A' || (unsigned char) i > 'Z'){
+			c=(unsigned char) i;
+		}else{
+			c=(unsigned char) tabinit[isintab(i,tabCipher,26)];
+		}
+		printf("%c",c);
+	}
+	return 0;
+}
