0bb03448a85757b16c4945ec2444f7fed59e8190
Benjamin Goodwin <goodb25@gmail.com>
2015-11-23 12:22:09 +0100

    Finished Network Project

--- Network/projet/client/tcp_client.c
+++ Network/projet/client/tcp_client.c
@@ -9,50 +9,52 @@
 #define MAX_SIZE 32 //Because 32 is the size of DATA in TCP datagramme
 
 int main(){
-	
+	//initialisation	
 	struct sockaddr_in serveur;
 	struct hostent *hp;
-	char mess_recu[BUFSIZ],mess_envoi[BUFSIZ], tab[BUFSIZ];
+	char mess_recu[MAX_SIZE],mess_envoi[MAX_SIZE], tab[MAX_SIZE];
 	int sock,cc,errno,i,j,n,l,d_sock,port;
 	char ip[16];
 	int longe,service;
-
-
+	//configuration IP & port
 	printf("Indiquez l'IP : ");
 	scanf("%s",&ip);
 	hp=gethostbyname(ip);
 	memcpy(&serveur.sin_addr,hp->h_addr,hp->h_length);
 	printf("Indiquez le numéro de port : ");
 	scanf("%i",&port);
-
+	//configuration socket
 	serveur.sin_port=port;
 	serveur.sin_family=AF_INET;
 	sock=socket(AF_INET,SOCK_STREAM,0);
-
+	//connexion au serveur
 	printf("Client ready\n");
 	l=connect(sock,(struct sockaddr *)&serveur,sizeof(serveur));
-	if(l==-1){
+	if(l==-1){//verification de la connexion établi
 		fprintf(stderr,"Connexion non établi, essayer de changer de port ou vérifier l'IP\n");
 		exit(1);
-	}else{
-		printf("Connected\n");
-		//reception du nom de fichier
-		read(sock,mess_recu,(sizeof(mess_recu)));
-		printf("Le client a reçu comme nom de fichier : %s \n",mess_recu);
-		//reception du fichier
-		FILE* fp=fopen(mess_recu,"w");
-		if (fp==NULL){
-			fprintf(stderr,"Error with file name");
-			exit(1);
-		}else{
-			strcpy(mess_recu,"");
-			n = read(sock,mess_recu,sizeof(mess_recu));
-			while(n > 0){
-				fwrite(mess_recu,sizeof(char)*MAX_SIZE,MAX_SIZE,fp);
-				n = read(sock,mess_recu,sizeof(mess_recu));
-			}
-			fclose(fp);
-		}
 	}
-	close(sock);
+	printf("Connected\n");
+	//reception du nom de fichier
+	read(sock,mess_recu,(sizeof(mess_recu)));
+	printf("Le client a reçu comme nom de fichier : %s \n",mess_recu);
+	//reception du fichier
+	FILE* fp=fopen(mess_recu,"w");
+	if (fp==NULL){//verification
+		fprintf(stderr,"Error with file name");
+		exit(1);
+	}
+	strcpy(mess_recu,"");//vidage du la variable par précaution
+	n = read(sock,mess_recu,sizeof(mess_recu));
+	int compteur=0+n;
+	while(n > 0){
+		fwrite(mess_recu,sizeof(char),MAX_SIZE,fp);
+		n = read(sock,mess_recu,sizeof(mess_recu));
+		compteur+=n;
+	}
+	fclose(fp);//fermeture du fichier
+	read(sock,mess_recu,sizeof(mess_recu));//reception du nombre de caractere
+	printf("Nombre de caractère envoyer =%s\n",mess_recu);
+	printf("Nombre de caractère recu = %i\n",compteur-MAX_SIZE);
+	close(sock);//fermeture
 }
--- Network/projet/server/tcp_serv.c
+++ Network/projet/server/tcp_serv.c
@@ -10,25 +10,25 @@
 
 
 int main(){
-	
-	char mess_recu[BUFSIZ],mess_envoi[BUFSIZ],nom[BUFSIZ];
+	//initialisation
+	char mess_recu[MAX_SIZE],mess_envoi[MAX_SIZE],nom[MAX_SIZE];
 	int pp,d_sock,cc,l,port;
 	int longe,service;
 	char tab[128];
-	
 	struct sockaddr_in serveur = {AF_INET};
 	struct sockaddr_in client;
-	
+	//configuration du port
 	printf("Indiquez le numéro de port : ");
 	scanf("%i",&port);
+	//configuration du Socket
 	serveur.sin_port=port;
 	serveur.sin_family=AF_INET;
 	serveur.sin_addr.s_addr=INADDR_ANY;
 	d_sock=socket(AF_INET,SOCK_STREAM,0);
-	
+	//launch socket
 	pp=bind(d_sock,(struct sockaddr*)&serveur,sizeof(serveur));
 	printf("Server ready\n");
-
+	//waiting for a connection
 	l=listen(d_sock,1);
 	longe=sizeof(struct sockaddr_in);
 	service=accept(d_sock,(struct sockaddr *)&client,&longe);
@@ -41,15 +41,24 @@ int main(){
 	printf("message envoyer : %s\n",mess_envoi);
 	//envoi du fichier
 	FILE* fp=fopen(nom,"r");
+	int compteur=0;
+	//verification fichier
 	if(fp==NULL){
 		fprintf(stderr,"File not Found\n");
 		exit(1);
-	}else{
-		while(!feof(fp)){
-			fread(mess_envoi,sizeof(char)*MAX_SIZE,MAX_SIZE,fp);
-			cc=write(service,mess_envoi,sizeof(mess_envoi));
-		}
-		fclose(fp);
 	}
+	//envoi
+	while(!feof(fp)){
+		fread(mess_envoi,sizeof(char),MAX_SIZE,fp);
+		cc=write(service,mess_envoi,sizeof(mess_envoi));
+		compteur+=cc;
+	}
+	//fermeture fichier
+	fclose(fp);
+	//envoi de la taille
+	sprintf(mess_envoi,"%d",compteur);
+	write(service,mess_envoi,sizeof(mess_envoi));
+	printf("Nombre de caractère envoyer ~= %s\n",mess_envoi);
+	//fermeture socket
 	close(d_sock);
 }
